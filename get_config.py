c8e3b8f099ad0668d181c2abf320380271b30b9f
pinterbanget <rjsecaccount@gmail.com>
2022-03-04 15:50:31 +0700

    added catch condition if config.txt file is missing (default values will be loaded instead)

--- get_config.py
+++ get_config.py
@@ -5,14 +5,19 @@ def get_config(file: str = 'config.txt') -> list:
     """
     default_values = [25, 5, 20, 4, 0, 1]
     time_config = []
-    with open(file, encoding='utf8') as f:
-        for line in f.readlines():
-            if len(time_config) == 6:
-                return time_config
-            try:
-                time_config.append(int(line.strip().split(': ')[1].split(' ')[0]))
-            except ValueError:
-                time_config.append(default_values[len(time_config)])
+    try:
+        with open(file, encoding='utf8') as f:
+            for line in f.readlines():
+                if len(time_config) == 6:
+                    return time_config
+                try:
+                    time_config.append(int(line.strip().split(': ')[1].split(' ')[0]))
+                except ValueError:
+                    time_config.append(default_values[len(time_config)])
 
-    return time_config
+        return time_config
+    except FileNotFoundError:
+        return default_values
+
+
 
